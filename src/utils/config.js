f97fa20e7ec818cb3372836cfe7d4eee2900ff1a
Amelia-Mowers <105567018+Amelia-Mowers@users.noreply.github.com>
2023-05-17 17:52:21 -0700

    modified default config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,14 @@
 const defaultConfig = {
-    "searchQueries": ["software developer", "software engineer"],
-    "location": "New York",
-    "numPages": 3,
-    "closeBrowser": true,
-    "headless": false,
-    "maxJobsDescToScrape": 2,
-    "maxConcurrentPageLimit": 3,
-    "api_key": "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",
-    "maxGptAnalysis": 1
+    searchQueries: ["software developer", "software engineer"],
+    location: "New York",
+    numPages: 2,
+    waitTimeMs: 5000,
+    closeBrowser: true,
+    headless: false,
+    maxJobsDescToScrape: 2,
+    maxConcurrentPageLimit: 3,
+    api_key: "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",
+    maxGptAnalysis: 1
 };
 
 const appConfigKey = 'AppConfig';
